1f91105cbb2d3ac2fab38318057ab0690ad96850
Stutzzz <leandrostutzsilva14@gmail.com>
2024-01-24 03:25:10 -0300

    adcionando o exercicioPensionato

---
+++ EXERCICIOS/exercicioPensionato/src/application/Program.java
@@ -0,0 +1,49 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Pessoa;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("How many rooms will be rented? ");
+	
+		int n = sc.nextInt();
+		
+		Pessoa[] p = new Pessoa[10];
+		
+		for (int i = 0;i<n;i++) {
+			System.out.printf("Rent #%d%n", i + 1);
+			
+			sc.nextLine();
+			System.out.print("Name: ");
+			String name = sc.nextLine();
+			
+			System.out.print("Email: ");
+			String email = sc.next();
+			
+			System.out.print("Room: ");
+			int room = sc.nextInt();
+			
+			p[room] = new Pessoa(name, email, room);
+		}
+		
+		System.out.println("Busy rooms: ");
+		for (int i = 0;i<p.length;i++) {
+			if (p[i] != null) {
+				System.out.printf("%d: ",i);
+				System.out.println(p[i]);
+			}
+		}
+		
+		
+		
+	}
+
+}
---
+++ EXERCICIOS/exercicioPensionato/src/entities/Pessoa.java
@@ -0,0 +1,43 @@
+package entities;
+
+public class Pessoa {
+	
+	private String nome;
+	private String email;
+	private int quarto;
+	
+	public Pessoa(String nome, String email, int quarto) {
+		this.nome = nome;
+		this.email = email;
+		this.quarto = quarto;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getQuarto() {
+		return quarto;
+	}
+
+	public void setQuarto(int quarto) {
+		this.quarto = quarto;
+	}
+	
+	public String toString() {
+		return nome + ", " + email; 
+	}
+
+}
